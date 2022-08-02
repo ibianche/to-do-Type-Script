9714f2ad13d040f4c660e748f90afd24a5195105
ibianche <inna.pomorska@gmail.com>
2022-08-02 15:41:03 +0200

    todolist

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+import {TodoItem} from "./todoItem";
+import {TodoCollection} from "./todoCollection";
+
+
+let todos = [
+    new TodoItem(1, "Kupić kwiaty"), new TodoItem(2, "Odebrać buty"),
+    new TodoItem(3, "Zamówić bilety"), new TodoItem(4, "Zadzwonić do Janka", true)];
+
+let collection = new TodoCollection ("Adam", todos);
+
 console.clear();
-console.log("Lista Adama");
+console.log('Lista ${collection.userName}a');
+
+let newId = collection.addTodo('Isc pobiegac');
+let todoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
+
