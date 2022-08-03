b821515ffc96dcab984965b355e1af04d2c8441a
ibianche <inna.pomorska@gmail.com>
2022-08-03 14:57:07 +0200

    r1

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Kupić kwiaty"), new todoItem_1.TodoItem(2, "Odebrać buty"),
+    new todoItem_1.TodoItem(3, "Zamówić bilety"), new todoItem_1.TodoItem(4, "Zadzwonić do Janka", true)
+];
+let collection = new todoCollection_1.TodoCollection("Adam", todos);
 console.clear();
-console.log("Lista Adama");
+console.log(`Lista ${collection.userName}a`);
+let newId = collection.addTodo('Isc pobiegac');
+let todoItem = collection.getTodoById(newId);
+// console.log(JSON.stringify(todoItem));
+todoItem.printDetails();
+collection.addTodo(todoItem);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import {TodoItem} from "./todoItem";
-import {TodoCollection} from "./todoCollection";
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
 
 
-let todos = [
+let todos: TodoItem[] = [
     new TodoItem(1, "Kupić kwiaty"), new TodoItem(2, "Odebrać buty"),
     new TodoItem(3, "Zamówić bilety"), new TodoItem(4, "Zadzwonić do Janka", true)];
 
-let collection = new TodoCollection ("Adam", todos);
+let collection: TodoCollection = new TodoCollection ("Adam", todos);
 
 console.clear();
-console.log('Lista ${collection.userName}a');
+console.log(`Lista ${collection.userName}a`);
 
-let newId = collection.addTodo('Isc pobiegac');
-let todoItem = collection.getTodoById(newId);
-console.log(JSON.stringify(todoItem));
+let newId: number = collection.addTodo('Isc pobiegac');
+let todoItem: TodoItem = collection.getTodoById(newId);
+// console.log(JSON.stringify(todoItem));
+todoItem.printDetails();
+
+collection.addTodo(todoItem);
+
 
